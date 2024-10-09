2a7badffea5945210955f46392c03416ec45df49
GabrielEmerson <gabrielemerson.dev@gmail.com>
2024-10-09 02:18:02 -0300

    feat: projeto vinculado ao MYSQL e com entidade funcionando

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 import { CreateUserDto } from './dtos/createUser.dto';
+import { Injectable } from '@nestjs/common';
+import { hash } from 'bcrypt';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
-  private users: User[] = [];
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<User> {
-    return {
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const saltOrRounds = 10;
+    const passwordHashed = await hash(createUserDto.password, saltOrRounds);
+
+    return this.userRepository.save({
       ...createUserDto,
-      id: 1,
-    };
+      password: passwordHashed,
+    });
+  }
+
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.userRepository.find();
   }
 }
